5617738ad8ce043bfc30141a187258f38322553d
VRVG14 <vivalago@hotmail.com>
2020-12-15 23:00:37 -0600

    Ahora si esta el .SQL con las tablas, creo

--- Tienda.sql
+++ Tienda.sql
@@ -1,10 +1,38 @@
 create database Tienda;
 
-create table Recargas (
-	id int primary key,
-    fecha date,
-    nombre varchar(50),
-    numeroTelefono varchar(10),
-    catidad float(0.0),
-    pagado boolean
+use Tienda;
+
+create table Producto (
+	idProducto int primary key check(idProducto > 0),
+    nombreProducto varchar(50) not null check(nombreProducto regexp '^[a-zA-Z0-9\\s]+$'),
+    nombreProveedor varchar(50) not null check(nombreProveedor regexp '^[a-zA-Z0-9\\s]+$'),
+    precio decimal(3,2) not null check(precio > 0),
+    cantidad int not null check(cantidad > -1),
+    unidadDeMedida varchar(30) not null check(unidadDeMedida regexp '^[a-zA-Z\\s]+$')
+);
+
+drop table cliente;
+create table cliente (
+	idCliente int not null check(idCliente > 0),
+    nombreCliente varchar(50) primary key check(nombreCliente regexp '^[a-zA-Z0-9\\s]+$'),
+    numeroCliente varchar(10) not null check(numeroCliente regexp '^[0-9\\s]+$')
+);
+
+create table compra(
+	idCompra int primary key check(idCompra > 0),
+    nombreCliente varchar(50) not null check(nombreCliente regexp '^[a-zA-Z0-9\\s]+$'),
+    fecha datetime not null,
+    total decimal(4,2) not null check(total > 0),
+    
+    foreign key(nombreCliente) references cliente(nombreCliente)
+);
+
+create table compraDetallada (
+	idCompraDetallada int primary key check(idCompraDetallada > 0),
+    idCompra int not null check(idCompra > 0),
+    idProducto int not null check(idProducto > 0),
+    subtotal decimal(4,2) not null check(subtotal > 0),
+    
+    foreign key(idCompra) references compra(idCompra),
+    foreign key(idProducto) references Producto(idProducto)
 );
